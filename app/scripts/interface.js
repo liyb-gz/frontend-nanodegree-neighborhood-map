ded4aba8b0533639edb51aded5b816ae09f9a8f0
Felix Li <lcfelixli@ust.hk>
2016-04-06 17:06:45 +0800

    prevent fiter <input> from submitting.

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -1,8 +1,12 @@
 // JS to support interface features only
 (function () {
 	'use strict';
-	$("#navbar-toggle").click(function(e) {
+	$('#navbar-toggle').click(function(e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+        $('#wrapper').toggleClass('toggled');
+    });
+
+    $('#filter-form').submit(function (e) {
+    	e.preventDefault();
     });
 })();
