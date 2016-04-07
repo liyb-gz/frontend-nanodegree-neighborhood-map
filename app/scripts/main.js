eb89300ad85f379bcdef655c84ecc154fd9d2fe9
Felix Li <lcfelixli@ust.hk>
2016-04-07 10:35:34 +0800

    changed structure. now the map and the viewmodel are in the same closure.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,57 +1,87 @@
 (function () {
 	'use strict';
 
+	var locations; //Model
+	var AppViewModel; //ViewModel
+	var map; //View 1
+	var markers = []; //View 2
+
+	AppViewModel = function (locations) {
+		var vm = this;
+		this.locations = locations;
+		this.currentLocation = ko.observable(this.locations()[0]); //Initially, show the first location
+		this.filter = ko.observable('');
+
+		//Compute the locations that needs to be displayed.
+		this.filteredLocations = ko.computed(function () {
+			return this.locations().filter(function (location) {
+				// Compare the location names with the given filter, case insensitively
+				return location.name.toLowerCase().indexOf(vm.filter().toLowerCase()) > -1;
+			});
+		}, this);
+
+		this.resetCurrentLocation = function () {
+			vm.currentLocation(this);
+		};
+
+		this.locations().forEach(function (location) {
+			location.isActive = ko.computed(function () {
+				return this === vm.currentLocation();
+			}, location);
+		});
+	}
+
+	// Load locations from the JSON file and preprocess.
+	function initLocations(locs) {
+		var locations = ko.observableArray(locs);
+
+		// Sort locations by alphabetical order.
+		locations().sort(function (a, b) {
+			if (a.name > b.name) {
+				return 1;
+			} else if (a.name < b.name) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+
+		return locations;
+	}
+
+	function initMap() {
+		var mapDiv = document.getElementById('map');
+
+		map = new google.maps.Map(mapDiv, {
+			center: {lat: -34.397, lng: 150.644},
+			zoom: 8
+		});
+
+		markers.push(new google.maps.Marker({
+			position: {
+				lat: 22.332040,
+				lng: 114.190232
+			},
+			map: map,
+			title: 'hello!'
+		}));
+
+		markers.push(new google.maps.Marker({
+			position: {
+				lat: 22.352040,
+				lng: 114.190232
+			},
+			map: map,
+			title: 'hello!'
+		}));
+	}
+
 	$.when(
 
 		// Load the locations
 		$.getJSON('scripts/models/locations.json', function(json) {
-			var locations = json.locations;
-
-			function AppViewModel() {
-				var vm = this;
-				this.locations = loadLocations(locations);
-				this.currentLocation = ko.observable(this.locations()[0]); //Initially, show the first location
-				this.filter = ko.observable('');
-
-				//Compute the locations that needs to be displayed.
-				this.filteredLocations = ko.computed(function () {
-					return this.locations().filter(function (location) {
-						// Compare the location names with the given filter, case insensitively
-						return location.name.toLowerCase().indexOf(vm.filter().toLowerCase()) > -1;
-					});
-				}, this);
-
-				this.resetCurrentLocation = function () {
-					vm.currentLocation(this);
-				};
-
-				this.locations().forEach(function (location) {
-					location.isActive = ko.computed(function () {
-						return this === vm.currentLocation();
-					}, location);
-				});
-			}
-
-			// Load locations from the JSON file.
-			function loadLocations(locs) {
-				var locations = ko.observableArray(locs);
-
-				// Sort locations by alphabetical order.
-				locations().sort(function (a, b) {
-					if (a.name > b.name) {
-						return 1;
-					} else if (a.name < b.name) {
-						return -1;
-					} else {
-						return 0;
-					}
-				});
-
-				return locations;
-			}
-
-			ko.applyBindings(new AppViewModel());
-
+			locations = initLocations(json.locations);
+			ko.applyBindings(new AppViewModel(locations));
 		}),
 
 		// Load the google map
@@ -61,9 +91,7 @@
 
 	// When both the locations and the google map are loaded, do the following
 	).then(function () {
-		// But how am I suppose to access the models here?
 		console.log('both loc and map loaded');
-
 	});
 
 })();
